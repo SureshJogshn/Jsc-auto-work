3affc521d60291bab0eab8622b7bc9e92c0b3781
SureshJogshn <sureshjogshn556@gmail.com>
2025-02-02 15:13:52 +0530

    jsc update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,13 @@ function App() {
     <>
       <div className='fixed top-0 left-0 w-full z-50 duration-300 bg-[#2C2F3A]'>
         <div className='max-w-[1240px] mx-auto flex items-center p-7 md:p-5 gap-[100px] justify-between md:justify-around'>
-          <div className=' flex items-center'>
+          <div className=' flex items-center gap-4'>
+            {
+
+              toggle ?
+                <AiOutlineMenu onClick={() => setToggle(!toggle)} className='text-2xl md:text-3xl text-white duration-100 cursor-pointer md:hidden block' />
+                : <AiOutlineClose onClick={() => setToggle(!toggle)} className='text-2xl md:text-3xl text-white duration-100 cursor-pointer md:hidden block' />
+            }
             <img src={logo} alt='logo' className='hidden w-[90px] md:w-[130px]' />
             <div className='text-xs sm:text-sm md:text-lg lg:text-xl uppercase text-white font-bold'> Jsc Auto Electric </div>
           </div>
@@ -29,14 +35,7 @@ function App() {
             <li ><ScrollLink smooth={true} duration={600} className='hover:border-b-2 border-[#FBFBFB] cursor-pointer' to="footer">Business Hub</ScrollLink></li>
             <li ><ScrollLink smooth={true} duration={600} className='hover:border-b-2 border-[#FBFBFB] cursor-pointer' to="/login">Login</ScrollLink></li>
           </ul>
-          {
-
-            toggle ?
-              <AiOutlineMenu onClick={() => setToggle(!toggle)} className='text-2xl md:text-3xl text-white duration-100
-              cursor-pointer md:hidden block' />
-              : <AiOutlineClose onClick={() => setToggle(!toggle)} className='text-2xl md:text-3xl text-white duration-100
-              cursor-pointer md:hidden block' />
-          }
+          <Link to="/login" className='hover:border-b-2 border-[#FBFBFB] cursor-pointer text-white'>Login</Link>
           {/* Responsive */}
           <ul className={`md:hidden bg-[#1E201E] fixed top-[85px] h-screen w-full left-0 duration-500
           ${!toggle ? `left-0` : `left-[-100%]`}`}>
