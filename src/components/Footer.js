5662348436a090c57d0244de063a8d43ff4ead87
SureshJogshn <sureshjogshn556@gmail.com>
2025-02-02 12:03:49 +0530

    jsc update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,23 +10,25 @@ const Footer = () => {
             <h1 className='corsor-pointer text-white hover:text-green-500 text-[16px] ml-5 md:text-[19px] font-semibold'><span>🔧</span> Our Services</h1>
             <div className='max-w-[1200px] p-3 md:p-4 rounded mx-auto flex flex-col gap-9 md:gap-[50px] shadow-sm shadow-black'>
                 <div className='flex flex-row justify-around md:gap-3'>
-                    <ul className='flex flex-row md:flex-col md:gap-2 md:w-[20px]'>
+                    <ul className='flex flex-row md:flex-col gap-2 md:gap-2 md:w-[20px]'>
                         <ul>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>ECM Repair</li>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Diagnostics</li>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Car Wiring</li>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Electrical Solutions</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Diagnostics</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Electrical Solutions</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>ECM Repair</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Car Wiring</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Self Motor Repair</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Remote Configuration</li>
                         </ul>
                         <ul>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Alternator Repair</li>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Self Motor Repair</li>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Key Programming</li>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Remote Configuration</li>
+
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Meter</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Dashboard Diagnostics</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>GPS Device Installation</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Key Programming</li>
+                            <li className='corsor-pointer text-white hover:text-green-500 text-[12px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Alternator Repair</li>
                         </ul>
                         <ul>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>GPS Device Installation</li>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Meter</li>
-                            <li className='corsor-pointer text-white hover:text-green-500 text-[13px] md:text-[18px]'> <span className='text-green-400'>✔ </span>Dashboard Diagnostics</li>
+
                         </ul>
                     </ul>
                     <div className='hidden md:block md:w-[400px]'>
@@ -35,8 +37,8 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className='flex flex-col gap-4 md:gap-5 justify-center jusitfy-center items-center'>
+                    <h2 className='text-[16px] md:text-[19px] text-white font-semibold'><span>📞 </span>Contact:</h2>
                     <ul className='flex flex-row gap-2 items-center md:[500px] '>
-                        <h2 className='text-[16px] md:text-[19px] text-white font-semibold'><span>📞 </span>Contact:</h2>
                         <p className='text-[13px] md:text-[16px] text-white '><span className='md:text-[20px]'>📲 </span> +91 9001323068</p>
                         <p className='text-[13px] md:text-[16px] text-white '><span className='md:text-[20px]'>📧 </span> nareshjogsan900@gmail.com</p>
                     </ul>
