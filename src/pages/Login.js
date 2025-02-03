8cae8bb8da93d6728774c270ab0737cd4dc72e77
SureshJogshn <sureshjogshn556@gmail.com>
2025-02-03 12:44:04 +0530

    update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,10 +4,10 @@ export default function Login() {
 
     return (
         <div className='w-full bg-[#2C2F3A] h-screen pt-[80px]'>
-            <h1 className='text-center text-white uppercase w-[350px] font-semibold p-2 mx-auto bg-gradient-to-r mt-[70px] from-white'>Login</h1>
-            <form className='flex flex-col gap-4 p-8 py-[100px] w-[350px] shadow-xl shadow-black h-[400px] mx-auto rounded bg-white mt-[20px]'>
-                <input type='text' className='border border-black p-2 rounded' placeholder='Enter the email' />
-                <input type='text' className='border border-black p-2 rounded' placeholder='Enter the phone number' />
+            <h1 className='text-center text-white uppercase w-[300px] font-semibold p-2 mx-auto bg-gradient-to-r mt-[70px] from-white'>Login</h1>
+            <form className='flex flex-col gap-4 p-8 py-[100px] w-[300px] shadow-xl shadow-black h-[400px] mx-auto rounded bg-white mt-[20px]'>
+                <input type='text' className='border border-black p-1 pl-2 rounded' placeholder='Enter the email' />
+                <input type='text' className='border border-black p-1 pl-2 rounded' placeholder='Enter the phone number' />
                 <button className='border border-2 bg-green-700 text-white py-2 mt-3 rounded'>Login</button>
                 <button className='text-blue-500 rounded'>forgot password</button>
             </form>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,13 +4,13 @@ export default function Register() {
 
     return (
         <div className='w-full bg-[#2C2F3A] h-screen pt-[80px]'>
-            <h1 className='text-center text-white uppercase w-[350px] font-semibold p-2 mx-auto bg-gradient-to-r mt-[40px] from-white'>Register</h1>
-            <form className='flex flex-col gap-4 p-6 py-[50px] w-[350px] shadow-xl shadow-black h-[450px] mx-auto rounded bg-white mt-[20px]'>
-                <input type='text' className='border border-black p-2 rounded' placeholder='Enter the username' />
-                <input type='text' className='border border-black p-2 rounded' placeholder='Enter the email' />
-                <input type='text' className='border border-black p-2 rounded' placeholder='Enter the phone number' />
-                <input type='text' className='border border-black p-2 rounded' placeholder='Enter the password' />
-                <input type='text' className='border border-black p-2 rounded' placeholder='Enter confirm password' />
+            <h1 className='text-center text-white uppercase w-[300px] font-semibold p-2 mx-auto bg-gradient-to-r mt-[40px] from-white'>Register</h1>
+            <form className='flex flex-col gap-4 p-6 py-[50px] w-[300px] shadow-xl shadow-black h-[420px] mx-auto rounded bg-white mt-[20px]'>
+                <input type='text' className='border border-black p-1 pl-3 rounded' placeholder='Enter the username' />
+                <input type='text' className='border border-black p-1 pl-3 rounded' placeholder='Enter the email' />
+                <input type='text' className='border border-black p-1 pl-3 rounded' placeholder='Enter the phone number' />
+                <input type='text' className='border border-black p-1 pl-3 rounded' placeholder='Enter the password' />
+                <input type='text' className='border border-black p-1 pl-3 rounded' placeholder='Enter confirm password' />
                 <button className='border border-2 bg-green-700 text-white py-2 mt-3 rounded'>Register</button>
             </form>
         </div>
