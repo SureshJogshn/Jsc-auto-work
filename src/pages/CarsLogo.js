0ba2c11ea2a853f38da9ebdaa090bfc658d8c98c
SureshJogshn <sureshjogshn556@gmail.com>
2025-02-13 12:42:45 +0530

    update

--- src/pages/CarsLogo.js
+++ src/pages/CarsLogo.js
@@ -1,9 +1,19 @@
 import React, { } from 'react'
 import { carlogo } from '../CarsLogoDetails';
+import { Link } from 'react-router-dom';
 export default function CarsLogo() {
     return (
-        <div className='w-full h-screen mt-[50px] md:mt-[68px] bg-[#2C2F3A] py-[30px]'>
-            <h1 className='text-2xl font-semibold text-white text-center py-5'>Maker</h1>
+        <div className='w-full h-screen mt-[110px] md:mt-[68px] bg-[#2C2F3A] py-[30px]'>
+            <h1 className='text-2xl font-semibold text-white text-center'>Maker</h1>
+            <div className='fixed top-[80px] bg-[#2C2F3A] left-0 w-full z-50'>
+                <h1 className='text-2xl font-semibold text-white text-center'>Maker</h1>
+                <ul className='fixed top-[110px] bg-[#2C2F3A] left-0 w-full flex flex-row text-white gap-5 items-center justify-center py-5'>
+                    <li><Link className='border-b-2 border-white'>Company's</Link></li>
+                    <li><Link className='hover:border-b-3 border-white'>Model</Link></li>
+                    <li><Link className='hover:border-b-3 border-white'>Year</Link></li>
+                    <li><Link className='hover:border-b-3 border-white'>Index</Link></li>
+                </ul>
+            </div>
             <div className='max-w-[1000px] border border-black mx-auto p-4 py-[50px] flex flex-row 
             items-center flex-wrap gap-5 bg-gray-300 border-2 rounded border-white overflow-x-scroll h-sreen'>
                 {carlogo.length > 0 ? (
