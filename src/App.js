29e455f22b9ce1ae791c80d700f4c8db25a886e2
SureshJogshn <sureshjogshn556@gmail.com>
2025-02-18 12:44:30 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
 import ContextData from './Data/ContextData';
 import CarsLogo from './pages/CarsLogo';
-import Model from './pages/Model';
-import Years from './pages/Years';
-import IndexData from './pages/IndexData';
+
 
 function App() {
   const [user, setUser] = useState(null);
@@ -34,9 +32,6 @@ function App() {
         <Route path="/login" element={user ? <Navigate to="/home" /> : <Login />} />
         <Route path="/read" element={user ? <ContextData> <ReadMore /> </ContextData> : <Navigate to="/login" />} />
         <Route path="/maker" element={user ? <CarsLogo /> : <Navigate to="/login" />} />
-        <Route path="/model" element={user ? <Model /> : <Navigate to="/login" />} />
-        {/* <Route path="/year" element={user ? <Years /> : <Navigate to="/login" />} />
-        <Route path="/indexs" element={user ? <IndexData /> : <Navigate to="/login" />} /> */}
       </Routes>
     </>
   );
