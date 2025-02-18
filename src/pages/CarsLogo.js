681e8b6e39c4ea613f2ab80319e85f93e6ec4908
SureshJogshn <sureshjogshn556@gmail.com>
2025-02-18 19:26:36 +0530

    update

--- src/pages/CarsLogo.js
+++ src/pages/CarsLogo.js
@@ -8,10 +8,10 @@ export default function CarsLogo() {
             items-center flex-wrap gap-8 bg-white border-2 rounded border-white overflow-x-scroll h-sreen'>
                 {carlogo.length > 0 ? (
                     carlogo.map((items, index) => (
-                        <div key={index} className='bg-[#F7F7F7] w-20 h-20 hover:scale-125 duration-500 
+                        <div key={index} className='bg-[#F7F7F7] w-20 h-20 mx-auto hover:scale-125 duration-500 
                         hover:shadow-green-500 rounded-lg cursor-pointer flex flex-col items-center justify-center
-                         p-2'>
-                            <img src={items.imgCar} alt='img' className='mt-3' />
+                         p-5'>
+                            <img src={items.imgCar} alt='img' />
                         </div>
                     ))
                 ) : (
--- src/pages/ReadMore.js
+++ src/pages/ReadMore.js
@@ -5,13 +5,13 @@ export default function ReadMore() {
     const userObj = useContext(DataContext) || [];  // ✅ Default empty array
 
     return (
-        <div className='w-full h-screen mt-[50px] md:mt-[68px] bg-[#2C2F3A] py-[30px]'>
-            <h1 className='text-2xl font-semibold text-white text-center py-5'>Our Services</h1>
-            <div className='max-w-[1000px] mx-auto p-4 py-[50px] flex flex-col gap-7 bg-gray-300 border-2 rounded border-white overflow-y-scroll h-[700px]'>
+        <div className='w-full h-screen mt-[50px] md:mt-[68px] bg-[#181818] py-[30px]'>
+            <h1 className='text-[16px] font-semibold text-white text-center py-5'>Our Services</h1>
+            <div className='max-w-[1000px] mx-auto p-4 py-[50px] flex flex-col gap-5 bg-gray-300 border-2 rounded border-white overflow-y-scroll h-[700px]'>
                 {userObj.length > 0 ? (
                     userObj.map((items, index) => (
-                        <div key={index} className='border-b bg-green-500 rounded-lg shadow-xl shadow-black '>
-                            <h2 className='text-xl font-bold text-white text-center p-4'>{items.title}</h2>
+                        <div key={index} className='bg-gradient-to-r from-blue-600 to-blue-300'>
+                            <h2 className='text-[16px] font-bold text-white text-center p-4'>{items.title}</h2>
                             <div className='bg-gray-200 p-4'>
                                 <p className='text-gray-600 py-2'>{items.about}</p>
                                 <ul className='list-disc pl-5 text-sm'>
