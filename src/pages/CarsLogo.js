1a4b91a19a94691b21ef01fd4b9738d40f4ec800
SureshJogshn <sureshjogshn556@gmail.com>
2025-02-18 19:14:02 +0530

    update

--- src/pages/CarsLogo.js
+++ src/pages/CarsLogo.js
@@ -5,13 +5,13 @@ export default function CarsLogo() {
     return (
         <div className='w-full mt-[70px] md:mt-[68px] py-[10px]'>
             <div className='max-w-[1200px] border border-black mx-auto p-4 py-[50px] flex flex-row 
-            items-center flex-wrap gap-5 bg-[#F8F5E9] border-2 rounded border-white overflow-x-scroll h-sreen'>
+            items-center flex-wrap gap-8 bg-white border-2 rounded border-white overflow-x-scroll h-sreen'>
                 {carlogo.length > 0 ? (
                     carlogo.map((items, index) => (
-                        <div key={index} className='hover:scale-125 duration-500 
-                        cursor-pointer flex flex-col items-center justify-center
-                         p-3'>
-                            <img src={items.imgCar} alt='img' className='w-[40px] mx-auto mt-3' />
+                        <div key={index} className='bg-[#F7F7F7] w-20 h-20 hover:scale-125 duration-500 
+                        hover:shadow-green-500 rounded-lg cursor-pointer flex flex-col items-center justify-center
+                         p-2'>
+                            <img src={items.imgCar} alt='img' className='mt-3' />
                         </div>
                     ))
                 ) : (
